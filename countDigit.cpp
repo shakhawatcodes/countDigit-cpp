4e0f2e6aa6f9a3275a521ce24e6d93e284c23610
MOHAMMAD SHAKHAWAT HOSSAIN <rhosain434@gmail.com>
2026-05-22 13:13:10 +0600

    Update countDigit.cpp

--- countDigit.cpp
+++ countDigit.cpp
@@ -1,17 +1,48 @@
-//programm  to Count the number of digits in a given number
+// Program to count the number of digits in a given number
+
 #include<iostream>
 using namespace std;
+
 int main(){
 
-int count=0;
-int n;
-cout<<" Dear User,Enter your  number: ";
+    char choice;
+    int n;
 
-cin>>n;
-while(n!=0){
-    n=n/10;
-    count++;
+    do{
+        int count = 0; // reset every time
 
+        cout << "Dear User, Enter your number: ";
+        cin >> n;
+
+        // Input validation
+        if(cin.fail()){
+            cout << "Invalid input!" << endl;
+            return 1;
+        }
+
+        // Negative handling
+        n = abs(n);
+
+        // Zero case
+        if(n == 0){
+            count = 1;
+        }
+        else{
+            while(n != 0){
+                n = n / 10;
+                count++;
+            }
+        }
+
+        cout << "Number of digits in your given number is: " 
+             << count << endl;
+
+        cout << "Try again? (y/n): ";
+        cin >> choice;
+
+    }while(choice == 'y');
+
+    return 0;
 }
 
 
@@ -20,5 +51,3 @@ while(n!=0){
 
 
 
-cout<<"Number of digits in your given number is: "<<count<<endl;
-}
